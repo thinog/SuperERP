d45af4101fe45014990357ae1cc09e05720a2a5d
thinog <thinomar@gmail.com>
2016-05-05 20:31:11 -0300

    Adicionado Interface de Repositorio

---
+++ SuperERP/SuperERP.Vendas/Listar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperERP.DAL.Repositories;
+
+namespace SuperERP.Vendas
+{
+    class Listar
+    {
+        public List<string> PessoaFisica()
+        {
+            List<string> pessoasFisicas;
+            PessoaFisicaRepositorio rep = new PessoaFisicaRepositorio();
+
+            return pessoasFisicas;
+        }
+    }
+}
