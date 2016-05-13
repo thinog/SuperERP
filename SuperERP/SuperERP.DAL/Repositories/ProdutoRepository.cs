bca3d7d2ac3f55a861941a0bdb305ec78183ec53
Eduardo Castro <educcjr@gmail.com>
2016-05-12 22:11:08 -0300

    criação e listagem de produto

---
+++ SuperERP/SuperERP.DAL/Repositories/ProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using SuperERP.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperERP.DAL.Context;
+
+namespace SuperERP.DAL.Repositories
+{
+    public class ProdutoRepository : Repositorio<Produto>
+    {
+    }
+}
