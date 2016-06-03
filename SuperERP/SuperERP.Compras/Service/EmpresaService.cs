5a72306c1a0420a63b7d93cbf2c346c613b52608
Alexandre de Lima <allima1991@gmail.com>
2016-06-03 01:04:27 -0300

    Services criadas e corrigidas

--- SuperERP/SuperERP.Compras/Service/EmpresaService.cs
+++ SuperERP/SuperERP.Compras/Service/EmpresaService.cs
@@ -11,9 +11,26 @@ namespace SuperERP.Compras.Service
 {
     public static class EmpresaService
     {
-        public static void Cadastrar(EmpresaDTO empresaDTO)
+        public static void Cadastrar(EmpresaDTO empresa)
         {
-            ServicoGenerico<Empresa, EmpresaDTO>.Cadastrar(empresaDTO);
+            ServicoGenerico<Empresa, EmpresaDTO>.Cadastrar(empresa);
+        }
+        public static void Alterar(EmpresaDTO empresa)
+        {
+            ServicoGenerico<Empresa, EmpresaDTO>.Alterar(empresa);
+        }
+        public static ICollection<EmpresaDTO> Listar()
+        {
+            return ServicoGenerico<Empresa, EmpresaDTO>.Listar();
+        }
+        public static void Deletar(int IdEmpresa)
+        {
+            ServicoGenerico<Empresa, EmpresaDTO>.Deletar(IdEmpresa);
+        }
+
+        public static EmpresaDTO BuscaPorId(int IdEmpresa)
+        {
+            return ServicoGenerico<Empresa, EmpresaDTO>.BuscaPorId(IdEmpresa);
         }
     }
 }
---
+++ SuperERP/SuperERP.Compras/Service/FuncionalidadeService.cs
@@ -0,0 +1,34 @@
+﻿using SuperERP.Compras.DTO;
+using SuperERP.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperERP.Compras.Service
+{
+    class FuncionalidadeService
+    {
+        public static void Cadastrar(FuncionalidadeDTO funcionalidade)
+        {
+            ServicoGenerico<Funcionalidade, FuncionalidadeDTO>.Cadastrar(funcionalidade);
+        }
+        public static void Alterar(FuncionalidadeDTO funcionalidade)
+        {
+            ServicoGenerico<Funcionalidade, FuncionalidadeDTO>.Alterar(funcionalidade);
+        }
+        public static ICollection<FuncionalidadeDTO> Listar()
+        {
+            return ServicoGenerico<Funcionalidade, FuncionalidadeDTO>.Listar();
+        }
+        public static void Deletar(int IdFuncionalidade)
+        {
+            ServicoGenerico<Funcionalidade, FuncionalidadeDTO>.Deletar(IdFuncionalidade);
+        }
+        public static FuncionalidadeDTO BuscaPorId(int IdFuncionalidade)
+        {
+            return ServicoGenerico<Funcionalidade, FuncionalidadeDTO>.BuscaPorId(IdFuncionalidade);
+        }
+    }
+}
