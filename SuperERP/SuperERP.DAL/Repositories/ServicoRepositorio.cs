a53c46d152d09d528d2f45f87f1738bcfbdfacd9
NicolasPedra <nicolas.rar@gmail.com>
2016-06-09 21:42:24 -0300

    Encerrando a criação

--- SuperERP/SuperERP.DAL/Repositories/ServicoRepositorio.cs
+++ SuperERP/SuperERP.DAL/Repositories/ServicoRepositorio.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 using SuperERP.DAL.Models;
 
 namespace SuperERP.DAL.Repositories
 {
-    class ServicoRepositorio : Repositorio<Servico>
+    public class ServicoRepositorio : Repositorio<Servico>
     {
+
+        public ICollection<Empresa> PegarEmpresa()
+        {
+            return dbContext.Empresas.ToList();
+        }
+
     }
 }
---
+++ SuperERP/SuperERP.Vendas/DTO/EmpresaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperERP.Vendas.DTO
+{
+   public class EmpresaDTO
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+
+    }
+}
