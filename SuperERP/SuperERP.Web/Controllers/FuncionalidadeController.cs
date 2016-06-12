9f8ed26d0e199b5c99ebfee6f9c8e3661653af8b
Alexandre de Lima <allima1991@gmail.com>
2016-06-12 13:07:20 -0300

    Crud da funcionalidade

---
+++ SuperERP/SuperERP.Web/Controllers/FuncionalidadeController.cs
@@ -0,0 +1,54 @@
+﻿using SuperERP.Compras.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperERP.Web.Controllers
+{
+    public class FuncionalidadeController : Controller
+    {
+        // GET: Funcionalidade
+        public ActionResult Index()
+        {
+            return View(Compras.Service.FuncionalidadeService.Listar());
+        }
+
+        public ActionResult Incluir()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Incluir(FuncionalidadeDTO funcionalidade)
+        {
+            Compras.Service.FuncionalidadeService.Cadastrar(funcionalidade);
+            return View();
+        }
+
+        public ActionResult Alterar(int funcionalidadeID)
+        {
+            return View(Compras.Service.FuncionalidadeService.BuscaPorId(funcionalidadeID));
+        }
+
+        [HttpPost]
+        public ActionResult Alterar(FuncionalidadeDTO funcionalidade)
+        {
+            Compras.Service.FuncionalidadeService.Alterar(funcionalidade);
+            return View();
+        }
+
+        public ActionResult Excluir(int funcionalidadeID)
+        {
+            return View(Compras.Service.FuncionalidadeService.BuscaPorId(funcionalidadeID));
+        }
+
+        [HttpPost]
+        public ActionResult Excluir(FuncionalidadeDTO funcionalidade)
+        {
+            Compras.Service.FuncionalidadeService.Deletar(funcionalidade.ID);
+            return View();
+        }
+    }
+}
---
+++ SuperERP/SuperERP.Web/Controllers/PerfilController.cs
@@ -0,0 +1,54 @@
+﻿using SuperERP.Compras.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperERP.Web.Controllers
+{
+    public class PerfilController : Controller
+    {
+        // GET: Perfil
+        public ActionResult Index()
+        {
+            return View(Compras.Service.PerfilService.Listar());
+        }
+
+        public ActionResult Incluir()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Incluir(PerfilDTO perfil)
+        {
+            Compras.Service.PerfilService.Cadastrar(perfil);
+            return View();
+        }
+
+        public ActionResult Alterar(int pefilID)
+        {
+            return View(Compras.Service.PerfilService.BuscaPorId(pefilID));
+        }
+
+        [HttpPost]
+        public ActionResult Alterar(PerfilDTO perfil)
+        {
+            Compras.Service.PerfilService.Alterar(perfil);
+            return View();
+        }
+
+        public ActionResult Excluir(int pefilID)
+        {
+            return View(Compras.Service.PerfilService.BuscaPorId(pefilID));
+        }
+
+        [HttpPost]
+        public ActionResult Excluir(PerfilDTO pefil)
+        {
+            Compras.Service.PerfilService.Deletar(pefil.ID);
+            return View();
+        }
+    }
+}
