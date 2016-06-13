08f63ff69ec07ef197c87e3f12fda36f26d78749
Alexandre de Lima <allima1991@gmail.com>
2016-06-13 19:35:53 -0300

    correção para merge com principal

--- SuperERP/SuperERP.DAL/Models/OrdemServico.cs
+++ SuperERP/SuperERP.DAL/Models/OrdemServico.cs
@@ -5,22 +5,22 @@ namespace SuperERP.DAL.Models
 {
     public partial class OrdemServico
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Nome { get; set; }
-        public int ID_Servico { get; set; }
-        public int ID_Status { get; set; }
-        public string Numero_Os { get; set; }
-        public System.DateTime DataI_Inicio { get; set; }
-        public System.DateTime DataI_Entrega { get; set; }
+        public int IdServico { get; set; }
+        public int IdStatus { get; set; }
+        public string NumeroOs { get; set; }
+        public DateTime DataI_Inicio { get; set; }
+        public DateTime DataI_Entrega { get; set; }
         public string Equipamento_Recebido { get; set; }
         public string NumeroSerie { get; set; }
         public string Marca { get; set; }
         public string Modelo { get; set; }
-        public string Obs_Recebimento { get; set; }
-        public string Obs_Problema { get; set; }
-        public string Descr_Servico { get; set; }
-        public string Obs_Interno { get; set; }
+        public string ObsRecebimento { get; set; }
+        public string ObsProblema { get; set; }
+        public string DescrServico { get; set; }
+        public string ObsInterno { get; set; }
         public virtual Servico Servico { get; set; }
-        public virtual StatusServico StatusServico { get; set; }
+        public virtual StatusServico Status_Servico { get; set; }
     }
 }
